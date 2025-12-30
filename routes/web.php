9b47a443f72f6a41d715f65912284d828bebcef6
riyansaefuloh <riyansaefuloh123@gmail.com>
2025-12-30 12:58:47 +0700

    perbaruan

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,25 @@ use App\Http\Controllers\PenggunaController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', function(){
+Route::get('/login', function(){
     return view('login');
-})->Name('login');
+})->name('login');
+
+Route::get('/', function(){
+    return redirect()->route('login');
+});
 
 Route::post('/proseslogin', [AuthController::class, 'proseslogin']);
 Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
 
-Route::resource('pengguna', PenggunaController::class);
 
-Route::middleware(['auth', 'role:owner'])->group(function () {
-    Route::resource('pengguna', PenggunaController::class);});
+Route::middleware(['auth', 'role:owner'])->prefix('pengguna')->group(function () {
+    Route::get('/', [PenggunaController::class, 'index'])->name('pengguna.index');
+    Route::get('/create', [PenggunaController::class, 'create'])->name('pengguna.create');
+    Route::post('/', [PenggunaController::class, 'store'])->name('pengguna.store');
+    Route::get('/{pengguna}/edit', [PenggunaController::class, 'edit'])->name('pengguna.edit');
+    Route::put('/{pengguna}', [PenggunaController::class, 'update'])->name('pengguna.update');
+    Route::delete('/{pengguna}', [PenggunaController::class, 'destroy'])->name('pengguna.destroy');
+});
 
 
